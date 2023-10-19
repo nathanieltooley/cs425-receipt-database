f9cc78980ed5b7644465ae0eea71f4e8a475450a
nathanieltooley <gotablast99@gmail.com>
2023-10-19 13:41:44 -0500

    Added file uploading to S3

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ import os
 import json
 
 from flask import Flask, Response, flash, request
+from receipt import Receipt
 from storage_hooks import AWSHook
 
 app = Flask(__name__)
@@ -24,8 +25,12 @@ def upload_receipt():
 
     if file:
         filename = file.filename
+        im_bytes = b''.join(file.stream.readlines())
         file.save(os.path.join(".temp", filename)) # type: ignore
-    
+        file.close()
+
+        aws = AWSHook()
+        aws.upload_receipt(Receipt(filename, im_bytes))
 
     return error_response(501, "ImplementationError", "Not Fully Implemented")
 

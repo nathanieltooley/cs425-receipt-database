3362ed936446924a063e75ff383ca78b61768bf1
nathanieltooley <gotablast99@gmail.com>
2023-10-19 01:01:35 -0500

    Added basic upload logic

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+
+from flask import Flask, Response, flash, request
 from storage_hooks import AWSHook
 
 app = Flask(__name__)
@@ -6,9 +8,21 @@ app = Flask(__name__)
 
 @app.route("/api/receipt/upload", method=["POST"])
 def upload_receipt():
-    return 404
+    if "file" not in request.files:
+        pass
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        pass
+
+    if file:
+        filename = file.filename
+        file.save(os.path.join(".", filename))
+
+    return Response("", status=501, mimetype="application/json")
 
 
 @app.route("/api/receipt/view")
 def view_receipt():
-    return 404
+    return Response("", status=501, mimetype="application/json")

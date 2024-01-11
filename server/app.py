f1bf539db1aa2292c9460aadd254b77750aa4be1
Nathaniel Tooley <gotablast99@gmail.com>
2024-01-11 13:37:47 -0600

    Added app.py documentation

--- server/app.py
+++ server/app.py
@@ -12,16 +12,35 @@ CORS(app)
 
 
 def error_response(status: int, error_name: str, error_message: str) -> Response:
+    """Create and return a Flask Response object that contains error information
+
+    Args:
+        status: HTML Status code to send with the response
+        error_name: Name of the error
+        error_message: Message to go along with the error
+
+    Returns:
+        Response
+    """
     res_json = {"error_name": error_name, "error_message": error_message}
     return Response(json.dumps(res_json), status=status, mimetype="application/json")
 
 
 def response_code(status: int) -> Response:
+    """Create an empty response with a status code
+
+    Args:
+        status: HTML Status code
+
+    Returns:
+        Response
+    """
     return Response("", status=status, mimetype="application/json")
 
 
 @app.route("/api/receipt/upload", methods=["POST"])
 def upload_receipt():
+    """API Endpoint for uploading a receipt image"""
     if "file" not in request.files:
         return error_response(400, "Missing Key", "The file has not been specified.")
 
@@ -49,6 +68,13 @@ def upload_receipt():
 
 @app.route("/api/receipt/view/<file_key>")
 def view_receipt(file_key: str):
+    """API Endpoint for viewing a receipt
+
+    This endpoint returns the bytes of the image to the caller
+
+    Args:
+        file_key: The AWS file name to view
+    """
     aws = AWSHook()
     receipt = aws.fetch_receipt(file_key)
     # NamedTemporaryFile will automatically delete itself on file close (by default)
@@ -63,6 +89,12 @@ def view_receipt(file_key: str):
 
 @app.route("/api/receipt/delete/<file_key>")
 def delete_receipt(file_key: str):
+    """Deletes a receipt in the AWS bucket
+
+    Args:
+        file_key: The AWS file name to delete
+
+    """
     aws = AWSHook()
     aws.delete_receipt_by_id(file_key)
 

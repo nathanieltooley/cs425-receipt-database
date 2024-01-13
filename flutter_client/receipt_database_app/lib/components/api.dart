0c2298417660222a06feb3eb8d2fc41ac7710b08
Robert-Portfolio <robert.borik@att.net>
2024-01-13 12:17:51 -0600

    Added login, setup, and inital landing pages and flow

---
+++ flutter_client/receipt_database_app/lib/components/api.dart
@@ -0,0 +1,30 @@
+// api.dart
+//import 'dart:convert';
+import 'package:http/http.dart' as http;
+//import 'package:connectivity/connectivity.dart';
+
+
+class Api {
+  static Future<void> uploadFile(String filePath) async {
+
+    try {
+      Uri uri = Uri.parse('http://127.0.0.1:5000/api/receipt/upload');
+      var request = http.MultipartRequest('POST', uri);
+
+      // Attach the file
+      request.files.add(await http.MultipartFile.fromPath('file', filePath));
+      
+      // Send the request
+      var response = await request.send();
+
+      // Check the response
+      if (response.statusCode == 200) {
+        //print('File uploaded successfully');
+      } else {
+        //print('Failed to upload file. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      //print('Error uploading file: $e');
+    }
+  }
+}
--- flutter_client/receipt_database_app/lib/main.dart
+++ flutter_client/receipt_database_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:receipt_database_app/pages/initial_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,47 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Receipt Database',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Receipt Database Homepage'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Upload receipts
-          },
-          child: Text('Upload Receipt'),
-        ),
-      ),
-    );
+      home: const InitialPage()
+      );
   }
 }

4871f940f6cf66a159b3376cda982c00fae8c971
IceMoonMagic <icemoonmagic@gmail.com>
2024-01-19 19:22:43 -0600

    Added object and relation for Tags

--- server/receipt.py
+++ server/receipt.py
@@ -1,6 +1,7 @@
 from datetime import datetime, UTC
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql.expression import func
 
 
@@ -9,12 +10,29 @@ class Base(DeclarativeBase):
     pass
 
 
+"""Intermediary table for many-to-many relationship between receipts and tags"""
+receipt_tag = Table(
+    "receipt_tag",
+    Base.metadata,
+    Column("tag_id", ForeignKey("tag.id"), primary_key=True),
+    Column("receipt_key", ForeignKey("receipt.key"), primary_key=True),
+)
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+
+
 class Receipt(Base):
     __tablename__ = 'receipt'
 
     key: Mapped[str] = mapped_column(primary_key=True)
     body: Mapped[bytes]
     _upload_dt: Mapped[datetime] = mapped_column(server_default=func.now())
+    tags: Mapped[list[Tag]] = relationship(secondary=receipt_tag)
 
     @property
     def upload_dt(self) -> datetime:

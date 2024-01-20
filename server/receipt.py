86ba5a53be5c365b2907c9ad498b2d68e6244811
IceMoonMagic <icemoonmagic@gmail.com>
2024-01-19 19:18:29 -0600

    Converted objects to Sqlalchemy

--- server/receipt.py
+++ server/receipt.py
@@ -1,11 +1,29 @@
-import dataclasses as dc
-import datetime as dt
+from datetime import datetime, UTC
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql.expression import func
 
 
-@dc.dataclass
-class Receipt:
-    key: str
-    body: bytes
-    upload_dt: dt.datetime = dc.field(
-        default_factory=lambda: dt.datetime.now().astimezone()
-    )
+class Base(DeclarativeBase):
+    """Used for actions regarding all tables"""
+    pass
+
+
+class Receipt(Base):
+    __tablename__ = 'receipt'
+
+    key: Mapped[str] = mapped_column(primary_key=True)
+    body: Mapped[bytes]
+    _upload_dt: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    @property
+    def upload_dt(self) -> datetime:
+        if self._upload_dt.tzinfo is None:  # Set to UTC if no timezone set
+            self._upload_dt = self.upload_dt.astimezone(UTC)
+        return self._upload_dt
+
+    @upload_dt.setter
+    def upload_dt(self, dt: datetime):
+        if not isinstance(dt, datetime):
+            raise ValueError(f'upload_dt must be datetime.datetime, got {type(dt)}')
+        self._upload_dt = dt.astimezone(UTC)  # Sets or converts to UTC datetime

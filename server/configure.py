df789d5752b2b570dd8fbe3e4d78d096fbfff13e
IceMoonMagic <icemoonmagic@gmail.com>
2024-01-21 21:22:16 -0600

    Added manager for configuration.
    Provides save, load, and backup.
    Doesn't provide validation.
    Basically a dict with a little extra

--- server/configure.py
+++ server/configure.py
@@ -1,8 +1,64 @@
 import argparse
+from datetime import datetime
 import importlib
+import json
+import os
 
+import platformdirs
 
 CURRENT_VERSION = "0.1.0-1.0"
+DIRS = platformdirs.PlatformDirs("Paperless", "Papertrail")
+
+
+class _ConfigManager:
+    FILE_PATH = os.path.normpath(DIRS.user_config_dir + "/config.json")
+
+    def __init__(self):
+        self.data = {}
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return repr(self.data)
+
+    def get(self, key, default=None):
+        """Get config section"""
+        return self.data.get(key, default)
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def set(self, key, value):
+        """Set config section"""
+        self.data[key] = value
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    @classmethod
+    def make_backup(cls):
+        if os.path.exists(cls.FILE_PATH):
+            time = datetime.now().astimezone()
+            time_str = time.isoformat(timespec="minutes")
+            os.rename(cls.FILE_PATH, f"{cls.FILE_PATH}.{time_str}.bak")
+            print(f"Config file backed up to {cls.FILE_PATH}.{time_str}.bak")
+
+    def save(self, make_backup: False):
+        os.makedirs(os.path.dirname(self.FILE_PATH), exist_ok=True)
+        if make_backup:
+            self.make_backup()
+        with open(self.FILE_PATH, "w") as file:
+            json.dump(self.__dict__, file, indent=4)
+            print(f"Config file saved to {self.FILE_PATH}")
+
+    def load(self):
+        with open(self.FILE_PATH) as file:
+            data = json.load(file)
+        self.__dict__.update(data)
+
+
+CONFIG = _ConfigManager()
 
 
 def make_parser() -> argparse.ArgumentParser:

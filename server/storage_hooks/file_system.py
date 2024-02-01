ce2d5c2b42b44ef7e03f37e9196dbb710417d0e4
Nathaniel Tooley <gotablast99@gmail.com>
2024-02-01 12:49:29 -0600

    Uses path.join

--- server/storage_hooks/file_system.py
+++ server/storage_hooks/file_system.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 import os
 
-from configure import CONFIG, DIRS
+from configure import CONFIG
 from storage_hooks.storage_hooks import FileHook
 
 UTC = timezone.utc
@@ -10,27 +10,31 @@ UTC = timezone.utc
 class FileSystemHook(FileHook):
     def __init__(self):
         config = CONFIG.FileSystem
-        self.file_path = os.path.dirname(config.file_path)
+        self.file_path = config.file_path
         os.makedirs(self.file_path, exist_ok=True)
 
     def save(self, image: bytes) -> str:
         # ToDo: Find choose appropriate key, may require more params
         key = datetime.now(UTC).isoformat(timespec="seconds")
-        with open(self.file_path + key, "wb+") as file:
+        with open(os.path.join(self.file_path, key), "wb+") as file:
             file.write(image)
         return key
 
     def replace(self, location: str, image: bytes):
-        if not os.path.exists(self.file_path + location):
-            raise FileNotFoundError(self.file_path + location)
-        with open(self.file_path + location, "wb+") as file:
+        r_path = os.path.join(self.file_path, location)
+
+        if not os.path.exists(r_path):
+            raise FileNotFoundError(r_path)
+        with open(r_path, "wb+") as file:
             file.write(image)
 
     def fetch(self, location: str) -> bytes:
-        with open(self.file_path + location, "rb") as file:
+        with open(os.path.join(self.file_path, location), "rb") as file:
             return file.read()
 
     def delete(self, location: str):
-        if not os.path.exists(self.file_path + location):
-            raise FileNotFoundError(self.file_path + location)
-        os.remove(self.file_path + location)
+        r_path = os.path.join(self.file_path, location)
+
+        if not os.path.exists(r_path):
+            raise FileNotFoundError(r_path)
+        os.remove(r_path)

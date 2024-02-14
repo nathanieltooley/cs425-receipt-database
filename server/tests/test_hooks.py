507bab63ddc8b0768b77ec872e7b5fea0a549878
IceMoonMagic <icemoonmagic@gmail.com>
2024-02-13 18:49:05 -0600

    Split apart test into multiple tests

--- server/tests/test_hooks.py
+++ server/tests/test_hooks.py
@@ -8,11 +8,11 @@ from storage_hooks.AWS import AWSS3Hook
 class TestFileHook:
     """Base class for hooks that store image files."""
 
-    @pytest.fixture()
+    @pytest.fixture
     def aws_s3_hook(self) -> AWSS3Hook:
-        yield AWSS3Hook()
+        return AWSS3Hook()
 
-    @pytest.fixture()
+    @pytest.fixture
     def file_system_hook(self) -> FileSystemHook:
         return FileSystemHook()
 
@@ -20,22 +20,38 @@ class TestFileHook:
     def hook(self, request) -> FileHook:
         return request.param()
 
-    def test_file_hook(self, hook: FileHook):
-        # Save and Read
+    @pytest.fixture
+    def save_file(self, hook) -> tuple[str, bytes]:
         file_name = "test_image1.png"
         with open("tests/" + file_name, "br") as test_image:
             test_bytes = test_image.read()
         save_key = hook.save(test_bytes, file_name)
+
+        yield save_key, test_bytes
+
+        try:
+            hook.delete(save_key)
+        except FileNotFoundError:
+            pass
+
+    def test_read(self, hook: FileHook, save_file):
+        save_key, test_bytes = save_file
         assert test_bytes == hook.fetch(save_key)
 
-        # Replace
+    def test_replace(self, hook: FileHook, save_file):
+        save_key, old_bytes = save_file
         file_name = "test_image2.png"
         with open("tests/" + file_name, "br") as test_image:
             test_bytes = test_image.read()
-        hook.replace(save_key, test_bytes)
-        assert test_bytes == hook.fetch(save_key)
+            assert test_bytes != old_bytes
 
-        # Delete
+        hook.replace(save_key, test_bytes)
+        fetched_bytes = hook.fetch(save_key)
+        assert test_bytes == fetched_bytes
+        assert old_bytes != fetched_bytes
+
+    def test_delete(self, hook: FileHook, save_file):
+        save_key, test_bytes = save_file
         hook.delete(save_key)
         with pytest.raises(FileNotFoundError):
             hook.fetch(save_key)

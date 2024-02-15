e0e25676d780b15c5ae4f3414bc7175a45937a5d
Robert-Portfolio <robert.borik@att.net>
2024-02-15 13:38:15 -0600

    Thumnails commited

--- flutter_client/receipt_database_app/lib/pages/database_page.dart
+++ flutter_client/receipt_database_app/lib/pages/database_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:file_picker/file_picker.dart';
 import '/components/api.dart';
 import '/pages/image_cache.dart' as MyImageCache; // Updated import statement
 import '/pages/view_receipt.dart';
 import 'dart:typed_data';
+import 'package:image/image.dart' as img;
 
 class MyListView extends StatefulWidget {
   const MyListView({super.key});
@@ -15,7 +15,7 @@ class MyListView extends StatefulWidget {
 
 class _MyListViewState extends State<MyListView> {
   List<Map<String, dynamic>> receiptDataList = [];
-   List<Map<String, dynamic>> filteredReceipts = [];
+  List<Map<String, dynamic>> filteredReceipts = [];
   final MyImageCache.ImageCache imageCache = MyImageCache.ImageCache();
   final TextEditingController _searchController = TextEditingController();
 
@@ -30,12 +30,14 @@ class _MyListViewState extends State<MyListView> {
     try {
       // Make the API call using fetchManyReceipts
       final apiService = Api();
-      final List<Map<String, dynamic>> receipts = await apiService.fetchManyReceipts();
+      final List<Map<String, dynamic>> receipts =
+          await apiService.fetchManyReceipts();
 
       // Iterate through the receipts and add them to receiptDataList
       for (final receipt in receipts) {
         // Store in cache using the ImageCache instance
-        await imageCache.storeBytesInCache(receipt['imageData'], receipt['title']);
+        await imageCache.storeBytesInCache(
+            receipt['imageData'], receipt['title']);
       }
 
       // Set the state to update the UI with the new data
@@ -52,9 +54,25 @@ class _MyListViewState extends State<MyListView> {
   void _filterReceipts(String query) {
     setState(() {
       filteredReceipts = receiptDataList
-          .where((receipt) => receipt['title'].toLowerCase().contains(query.toLowerCase()))
+          .where((receipt) =>
+              receipt['title'].toLowerCase().contains(query.toLowerCase()))
           .toList();
     });
+  }
+
+  Future<Uint8List?> generateThumbnail(Uint8List imageData) async {
+    try {
+      img.Image? image = img.decodeImage(imageData);
+      if (image == null) {
+        print('Error decoding image.'); // Handle decoding error
+        return null;
+      }
+      img.Image thumbnail = img.copyResize(image, width: 100, height: 100);
+      return Uint8List.fromList(img.encodePng(thumbnail));
+    } catch (e) {
+      print('Error generating thumbnail: $e'); // Handle thumbnail generation error
+      return null;
+    }
   }
 
   @override
@@ -76,7 +94,8 @@ class _MyListViewState extends State<MyListView> {
             itemCount: filteredReceipts.length,
             itemBuilder: (context, index) {
               final title = filteredReceipts[index]['title'] as String;
-              final imageData = filteredReceipts[index]['imageData'] as Uint8List;
+              final imageData =
+                  filteredReceipts[index]['imageData'] as Uint8List;
 
               return Card(
                 elevation: 5,
@@ -94,10 +113,22 @@ class _MyListViewState extends State<MyListView> {
                       ),
                     );
                   },
-                  leading: CachedNetworkImage(
-                    imageUrl: filteredReceipts[index]['imageUrl'] as String,
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  leading: FutureBuilder<Uint8List?>(
+                    future: generateThumbnail(imageData),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.hasError || snapshot.data == null) {
+                        return const Icon(Icons.error_outline);
+                      } else {
+                        return Image.memory(
+                          snapshot.data!,
+                          fit: BoxFit.cover,
+                          width: 80, // Set the width for the thumbnail
+                          height: 80, // Set the height for the thumbnail
+                        );
+                      }
+                    },
                   ),
                 ),
               );

4622423d4d65029d3e373627a3dc1b894d198ddc
nathanieltooley <gotablast99@gmail.com>
2024-02-20 15:22:06 -0600

    flask tests now use temp hooks

--- server/tests/test_flask_unit.py
+++ server/tests/test_flask_unit.py
@@ -11,15 +11,14 @@ import pathlib
 file_path = pathlib.Path(__file__).absolute()
 sys.path.append(str(file_path.parent.parent))
 from storage_hooks.SQLite3 import SQLite3
-from storage_hooks.file_system import FileSystemHook
-from storage_hooks.storage_hooks import FileHook, StorageHook
+from tests.temp_hooks import MemorySQLite3, FileSystemHook
 
 
 @pytest.fixture()
 def app():
     from app import create_app
 
-    app = create_app(FileSystemHook(), SQLite3())
+    app = create_app(FileSystemHook(), MemorySQLite3())
 
     app.config.update(
         {
@@ -49,7 +48,7 @@ def test_upload_receipt(test_client: FlaskClient, mocker):
     mocker.patch(
         "storage_hooks.storage_hooks.DatabaseHook.create_receipt", return_value=1
     )
-    response = test_client.post("/api/receipt/upload", data=data)
+    response = test_client.post("/api/receipt/", data=data)
     response_json = cast(Any, response.json)
 
     assert response.status_code == 200
@@ -60,7 +59,7 @@ def test_upload_receipt(test_client: FlaskClient, mocker):
 
 
 def test_upload_receipt_missing_file_key(test_client: FlaskClient):
-    response = test_client.post("/api/receipt/upload")
+    response = test_client.post("/api/receipt/")
     response_json = cast(Any, response.json)
     assert response.status_code == 404
     assert response_json["error_name"] == "Missing Key"
@@ -70,7 +69,7 @@ def test_upload_receipt_missing_file_data(test_client: FlaskClient):
     data = {}
     data["file"] = (io.BytesIO(b""), "test.jpg")
 
-    response = test_client.post("/api/receipt/upload", data=data)
+    response = test_client.post("/api/receipt/", data=data)
 
     assert response.status_code == 404
     assert cast(Any, response.json)["error_name"] == "Missing File"

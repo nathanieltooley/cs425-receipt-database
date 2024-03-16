cc71ce21f249e01243c0afcfd85576bcb07f99dc
nathanieltooley <gotablast99@gmail.com>
2024-03-15 20:09:06 -0500

    Fixed error where filesystem hook would delete test sqlite db

--- server/tests/temp_hooks.py
+++ server/tests/temp_hooks.py
@@ -11,6 +11,7 @@ from storage_hooks.storage_hooks import DatabaseHook
 
 class MemorySQLite3(DatabaseHook):
     """Like sqlite3 but in raw memory rather a memory only file."""
+
     def __init__(self):
         super().__init__()
         self.engine = create_engine(f"sqlite://")
@@ -19,7 +20,7 @@ class MemorySQLite3(DatabaseHook):
 def sqlite3() -> SQLite3:
     """Make a SQLite3 db stored in the runtime dir a.k.a. temp."""
     runtime_dir = DIRS.user_runtime_dir
-    filename = "receipts.test.sqlite3"
+    filename = "test_receipts.sqlite3"
     CONFIG.SQLite3.db_path = os.path.join(runtime_dir, filename)
     return SQLite3()
 
